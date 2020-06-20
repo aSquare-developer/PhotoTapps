4d85c2e7909022003b3c22222321d41185bf68d0
Artur <artur.anissimov@gmail.com>
2020-06-21 00:41:34 +0300

    CollectionView Configuration

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Artur Anissimov on 21.06.2020.
+//  Copyright © 2020 Artur Anissimov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
