39217dc07bf1f092504dc506561369b943e85b7d
Artur <artur.anissimov@gmail.com>
2020-06-21 01:31:16 +0300

    Detail Screen Setup

---
+++ PhotoTapps/PhotoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoViewController.swift
+//  PhotoTapps
+//
+//  Created by Artur Anissimov on 21.06.2020.
+//  Copyright © 2020 Artur Anissimov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    var image: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        photoImageView.image = image
+    }
+    
+    @IBAction func shareAction(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
